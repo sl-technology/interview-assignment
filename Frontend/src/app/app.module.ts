8a6890ad77895a0ee308db3a2ae03753cff8897a
Ruben Brugman <ruben.brugman@spendlab.com>
2023-11-17 14:51:11 +0100

    Added frontend

---
+++ Frontend/src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, MatToolbarModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
